f43a1e9a2582ba09e0f76d12fed5b947ca5f4836
Pekka Savolainen <pekka.t.savolainen@vtt.fi>
2015-10-23 18:45:27 +0300

    Example dummy classes

---
+++ titan/configuration_layer/configuration_reader.py
@@ -0,0 +1,15 @@
+"""
+Another example dummy class for the configuration layer.
+
+:author: Pekka Savolainen <pekka.t.savolainen@vtt.fi>
+:date:   22.10.2015
+"""
+
+
+class ConfigEditor():
+
+    def __init__(self):
+        pass
+
+    def read(self):
+        pass
