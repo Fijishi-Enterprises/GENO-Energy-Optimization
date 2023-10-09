acc05d825f5e85ce5d9ec0288d3e2152143f52fd
Niina Helistö <niina.helisto@vtt.fi>
2023-10-09 21:19:34 +0300

    Cleaning up the invest and schedule run script

--- run-invest-and-schedule-TradeRES.cmd
+++ run-invest-and-schedule-TradeRES.cmd
@@ -1,8 +1,6 @@
 Title Running scenario: "backbone TradeRES model - invest and schedule"
 
 :: investment run
-:: convert input excel to input gdx
-::C:\GAMS\win64\24.1\gdxxrw Input="./input/TradeRES/InputData_traderes_run3.xlsx" Output="./input/TradeRES/inputData.gdx" CheckDate Index=INDEX!
 
 :: preparing additional input data files
 copy .\input\TradeRES\4d_postProcess_invest.gms .\input\TradeRES\4d_postProcess.gms
@@ -12,9 +10,10 @@ copy .\input\TradeRES\cplex_invest.opt .\cplex.opt
 
 :: running backbone in invest mode
 for /F "tokens=3" %%A in ('reg query "HKEY_CURRENT_USER\gams.location"') DO (%%A\gams.exe Backbone.gms --input_dir=./input/TradeRES --init_file=investInit.gms --debug=1 -lo=2 --penalty=4000 --resultsVer2x=True)
-
+::C:\GAMS\win64\24.1\gams.exe Backbone.gms --input_dir=./input/TradeRES --init_file=investInit.gms --debug=1 -lo=2 --penalty=4000 --resultsVer2x=True
+::<insert your GAMS call here> Backbone.gms --input_dir=./input/TradeRES --init_file=investInit.gms --debug=1 -lo=2 --penalty=4000 --resultsVer2x=True
 :: For VTT users: alternative way to call Backbone when using Hassle (please set your GAMS version in the path below)
-:: CALL "C:\Program Files\VTT\HASSLE\hassle.exe" C:\GAMS\44\gams.exe %~dp0Backbone.gms --input_dir=./input/TradeRES --init_file=investInit.gms --debug=1 -lo=2 --penalty=4000 --resultsVer2x=True
+::CALL "C:\Program Files\VTT\HASSLE\hassle.exe" C:\GAMS\44\gams.exe %~dp0Backbone.gms --input_dir=./input/TradeRES --init_file=investInit.gms --debug=1 -lo=2 --penalty=4000 --resultsVer2x=True
 
 :: copying summary of invest results to TradeRES folder
 copy .\output\invest_results.inc .\input\TradeRES\invest_results.inc
@@ -25,24 +24,19 @@ copy .\Backbone.lst .\output\lst-invest.lst
 
 
 :: schedule run
-:: using the same input gdx, no need to convert it again
-:: preparing additional input data files
+
+: preparing additional input data files
 del .\input\TradeRES\4d_postProcess.gms
 del .\input\TradeRES\changes.inc
 copy .\input\TradeRES\changes-schedule.inc .\input\TradeRES\changes.inc
 copy .\input\TradeRES\cplex_schedule.opt .\cplex.opt
 
-
 :: running backbone in schedule mode
 for /F "tokens=3" %%A in ('reg query "HKEY_CURRENT_USER\gams.location"') DO (%%A\gams.exe Backbone.gms --input_dir=./input/TradeRES --init_file=scheduleInit.gms --debug=1 -lo=2 --penalty=4000 --resultsVer2x=True)
-
+::C:\GAMS\win64\24.1\gams.exe Backbone.gms --input_dir=./input/TradeRES --init_file=scheduleInit.gms --debug=1 -lo=2 --penalty=4000 --resultsVer2x=True
+::<insert your GAMS call here> Backbone.gms --input_dir=./input/TradeRES --init_file=scheduleInit.gms --debug=1 -lo=2 --penalty=4000 --resultsVer2x=True
 :: For VTT users: alternative way to call Backbone when using Hassle (please set your GAMS version in the path below)
-:: CALL "C:\Program Files\VTT\HASSLE\hassle.exe" C:\GAMS\44\gams.exe %~dp0Backbone.gms --input_dir=./input/TradeRES --init_file=scheduleInit.gms --debug=1 -lo=2 --penalty=4000 --resultsVer2x=True
-
-:: copying result files to demo1 folder
-::copy .\output\results.gdx .\output\results-investAndSchedule.gdx
-::copy .\output\debug.gdx .\output\debug-investAndSchedule.gdx
-
+::CALL "C:\Program Files\VTT\HASSLE\hassle.exe" C:\GAMS\44\gams.exe %~dp0Backbone.gms --input_dir=./input/TradeRES --init_file=scheduleInit.gms --debug=1 -lo=2 --penalty=4000 --resultsVer2x=True
 
 :: removing additional input data 
 del .\input\TradeRES\changes.inc
